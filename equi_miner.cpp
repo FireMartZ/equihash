47d859a23344fa63313cbbbd6a8040908543c10c
FireMartZ <martindube@martindube.com>
2018-03-13 19:58:34 -0400

    Modify to show encoded (packed) solution. Allow to change nonce double word position. Change output to be easier to parse with GenesisZ

--- equi_miner.cpp
+++ equi_miner.cpp
@@ -15,6 +15,63 @@ int hextobyte(const char * x) {
   return b;
 }
 
+/*
+** Write ZCASH_SOL_LEN bytes representing the encoded solution as per the
+** Zcash protocol specs (512 x 21-bit inputs).
+**
+** out		ZCASH_SOL_LEN-byte buffer where the solution will be stored
+** inputs	array of 32-bit inputs
+** n		number of elements in array
+*/
+#define PREFIX (DIGITBITS)
+void store_encoded_sol(uint8_t *out, uint32_t *inputs, uint32_t n)
+{
+    uint32_t byte_pos = 0;
+    int32_t bits_left = PREFIX + 1;
+    uint8_t x = 0;
+    uint8_t x_bits_used = 0;
+    while (byte_pos < n)
+      {
+        if (bits_left >= 8 - x_bits_used)
+          {
+            x |= inputs[byte_pos] >> (bits_left - 8 + x_bits_used);
+            bits_left -= 8 - x_bits_used;
+            x_bits_used = 8;
+          }
+        else if (bits_left > 0)
+          {
+            uint32_t mask = ~(-1 << (8 - x_bits_used));
+            mask = ((~mask) >> bits_left) & mask;
+            x |= (inputs[byte_pos] << (8 - x_bits_used - bits_left)) & mask;
+            x_bits_used += bits_left;
+            bits_left = 0;
+          }
+        else if (bits_left <= 0)
+          {
+            assert(!bits_left);
+            byte_pos++;
+            bits_left = PREFIX + 1;
+          }
+        if (x_bits_used == 8)
+          {
+	    *out++ = x;
+            x = x_bits_used = 0;
+          }
+      }
+}
+
+#define ZCASH_SOL_LEN                   ((1 << WK) * (PREFIX + 1) / 8)
+void print_encoded_sol(uint32_t *inputs, uint32_t n)
+{
+    uint8_t	sol[ZCASH_SOL_LEN];
+    uint32_t	i;
+    store_encoded_sol(sol, inputs, n);
+    for (i = 0; i < sizeof (sol); i++)
+	printf("%02x", sol[i]);
+    printf("\n");
+    fflush(stdout);
+}
+
 int main(int argc, char **argv) {
   int nthreads = 1;
   int nonce = 0;
@@ -22,17 +79,21 @@ int main(int argc, char **argv) {
   bool showsol = false;
   const char *header = "";
   const char *hex = "";
+  int noncepos = 32;
   int c;
-  while ((c = getopt (argc, argv, "h:n:r:t:x:s")) != -1) {
+  while ((c = getopt (argc, argv, "p:h:n:r:t:x:s")) != -1) {
     switch (c) {
       case 'h':
         header = optarg;
+        printf("Header specified: %s\n", header);
         break;
       case 'n':
         nonce = atoi(optarg);
+        printf("Nonce specified: %d\n", nonce);
         break;
       case 'r':
         range = atoi(optarg);
+        printf("Range specified: %d\n", range);
         break;
       case 's':
         showsol = true;
@@ -42,9 +103,14 @@ int main(int argc, char **argv) {
         break;
       case 'x':
         hex = optarg;
+        printf("Hex specified: %s\n", hex);
+        break;
+      case 'p':
+        noncepos = atoi(optarg);
         break;
     }
   }
+  printf("NoncePos: %d\n", noncepos);
 #ifndef XWITHASH
   if (sizeof(tree) > 4)
     printf("WARNING: please compile with -DXWITHASH to shrink tree!\n");
@@ -77,9 +143,11 @@ int main(int argc, char **argv) {
     memcpy(headernonce, header, hdrlen);
     memset(headernonce+hdrlen, 0, sizeof(headernonce)-hdrlen);
   }
+
   for (int r = 0; r < range; r++) {
-    ((u32 *)headernonce)[32] = htole32(nonce+r);
+    ((u32 *)headernonce)[noncepos] = htole32(nonce+r);
     eq.setheadernonce(headernonce, sizeof(headernonce));
+
     for (int t = 0; t < nthreads; t++) {
       threads[t].id = t;
       threads[t].eq = &eq;
@@ -93,12 +161,23 @@ int main(int argc, char **argv) {
     u32 nsols, maxsols = min(MAXSOLS, eq.nsols);
     for (nsols = 0; nsols < maxsols; nsols++) {
       if (showsol) {
-        printf("\nSolution");
+        printf("\nSolution [%d]\n", PROOFSIZE);
         for (u32 i = 0; i < PROOFSIZE; i++)
           printf(" %jx", (uintmax_t)eq.sols[nsols][i]);
+
+        printf("\nEncoded\n");
+        print_encoded_sol(eq.sols[nsols], 1 << WK);
       }
     }
-    printf("\n%d solutions\n", nsols);
+    if (maxsols > 0)
+    {
+      printf("\nNonce ");
+      for (int i = 0; i < 32; ++i)
+      {
+        printf("%02x", (unsigned char)headernonce[108+i]);
+      }
+      printf(" %d solutions\n", nsols);
+    }
     sumnsols += nsols;
   }
   free(threads);
